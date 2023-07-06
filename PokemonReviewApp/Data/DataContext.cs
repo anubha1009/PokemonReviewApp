bbcf0f20be192b284eb075c642c1f83abfc383b6
anubha1009 <axv64880@ucmo.edu>
2023-07-05 22:08:04 -0500

    Added DB Context and DB set as models . to link Database to the app

---
+++ PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options): base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }  
+    }
+}
